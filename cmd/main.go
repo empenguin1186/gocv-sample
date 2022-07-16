27bb1f625d9423b6610d6b8375b0b629e354b106
empenguin1186 <emperorpenguint1186@gmail.com>
2022-07-16 14:00:54 +0900

    :+1: 顔検出処理実装

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,35 @@ package main
 import (
 	"fmt"
 	"gocv.io/x/gocv"
+	"log"
+	"time"
 )
 
 func main() {
-	fmt.Printf("gocv version: %s\n", gocv.Version())
-	fmt.Printf("opencv lib version: %s\n", gocv.OpenCVVersion())
+	// load classifier file for face detection
+	xmlFile := "./data/haarcascade_frontalface_default.xml"
+	classifier := gocv.NewCascadeClassifier()
+	defer classifier.Close()
+	if !classifier.Load(xmlFile) {
+		log.Fatalf("Error reading cascade file: %v\n", xmlFile)
+	}
+
+	start := time.Now().UnixNano()
+
+	// load face image
+	filename := "./images/sample.png"
+	img := gocv.IMRead(filename, gocv.IMReadColor)
+	if img.Empty() {
+		log.Fatalf("Error reading image from: %v\n", filename)
+	}
+	defer img.Close()
+
+	// execute face detection
+	rects := classifier.DetectMultiScale(img)
+
+	end := time.Now().UnixNano()
+
+	fmt.Printf("found %d faces\n", len(rects))
+	fmt.Printf("start time: %d nano seconds\n", start)
+	fmt.Printf("end time: %d nano seconds\n", end)
 }
