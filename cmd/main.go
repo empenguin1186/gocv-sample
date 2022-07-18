7f693c30931334dd57e68531cb408b830323629f
empenguin1186 <emperorpenguint1186@gmail.com>
2022-07-18 13:50:27 +0900

    :shower: 不要なコード削除

--- cmd/main.go
+++ cmd/main.go
@@ -7,32 +7,6 @@ import (
 )
 
 func main() {
-	//// load classifier file for face detection
-	//xmlFile := "./data/haarcascade_frontalface_default.xml"
-	//classifier := gocv.NewCascadeClassifier()
-	//defer classifier.Close()
-	//if !classifier.Load(xmlFile) {
-	//	log.Fatalf("Error reading cascade file: %v\n", xmlFile)
-	//}
-	//
-	//start := time.Now().UnixNano()
-	//
-	//// load face image
-	//filename := "./images/sample.png"
-	//img := gocv.IMRead(filename, gocv.IMReadColor)
-	//if img.Empty() {
-	//	log.Fatalf("Error reading image from: %v\n", filename)
-	//}
-	//defer img.Close()
-	//
-	//// execute face detection
-	//rects := classifier.DetectMultiScale(img)
-	//
-	//end := time.Now().UnixNano()
-	//
-	//fmt.Printf("found %d faces\n", len(rects))
-	//fmt.Printf("start time: %d nano seconds\n", start)
-	//fmt.Printf("end time: %d nano seconds\n", end)
 	log.Printf("Server started")
 
 	classifierFileName := "./data/haarcascade_frontalface_default.xml"
